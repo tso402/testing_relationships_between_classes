e49a0d0bf8a4a23b9606343598d91d1bcee90e7a
James Ault <james.ault@johnlewis.co.uk>
2019-02-08 10:29:30 +0000

    completed rspec with passing tests

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -19,14 +19,37 @@ require_relative '../lib/secret_diary'
 
 RSpec.describe SecretDiary do
   context "when locked" do
-    pending "refuses to be read"
+    #pending "refuses to be read"
+    it 'refuses to be read' do
+      james_diary = SecretDiary.new(james_diary)
+      expect(james_diary.read).to eq "Go away!"
+  end
 
-    pending "refuses to be written"
+    #pending "refuses to be written"
+    it 'refuses to be written' do
+      james_diary = SecretDiary.new(james_diary)
+      expect(james_diary.write('Hello, James')).to eq "Go away!"
+    end
   end
 
   context "when unlocked" do
-    pending "gets read"
+    #pending "gets read"
+    it "gets read" do
+      james_diary = double('awda')
 
-    pending "gets written"
+      diary = SecretDiary.new(james_diary)
+      diary.unlock
+      expect(james_diary).to receive(:read)
+      diary.read
+
+    end
+
+    it "gets written" do
+      james_diary = double('diary')
+      diary = SecretDiary.new(james_diary)
+      diary.unlock
+      expect(james_diary).to receive(:write)
+      diary.write('This Thing')
+    end
   end
 end
